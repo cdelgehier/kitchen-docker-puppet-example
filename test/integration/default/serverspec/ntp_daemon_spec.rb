4946c75099a7f086eb94898ded89bb1e10fd23c3
Delgehier Cédric <cedric.delgehier@gmail.com>
2015-04-10 01:01:30 +0200

    correction des ntp server

--- test/integration/default/serverspec/ntp_daemon_spec.rb
+++ test/integration/default/serverspec/ntp_daemon_spec.rb
@@ -9,48 +9,6 @@ end
 
 describe file('/etc/ntp.conf') do
   it { should be_file }
-  its(:content) { should match /server scl-intp01.intcs.meshcore.net prefer/ }
-  its(:content) { should match /server brx-intp01.intcs.meshcore.net/ }
-  its(:content) { should match /server brx-intp02.intcs.meshcore.net/ }
-  its(:content) { should match /server fkf-intp01.intcs.meshcore.net/ }
-end
-
-describe "Ntp Daemon" do
-  it "has a running service of ntpd" do
-    expect(service("ntpd")).to be_running
-  end
-end
-require 'serverspec'
-
-# Required by serverspec
-set :backend, :exec
-
-describe package('ntp'), :if => os[:family] == 'redhat' do
-  it { should be_installed }
-end
-
-describe file('/etc/ntp.conf') do
-  it { should be_file }
-  its(:content) { should match /server 0.pool.ntp.org prefer/ }
-  its(:content) { should match /server 1.pool.ntp.org/ }
-end
-
-describe "Ntp Daemon" do
-  it "has a running service of ntpd" do
-    expect(service("ntpd")).to be_running
-  end
-end
-require 'serverspec'
-
-# Required by serverspec
-set :backend, :exec
-
-describe package('ntp'), :if => os[:family] == 'redhat' do
-  it { should be_installed }
-end
-
-describe file('/etc/ntp.conf') do
-  it { should be_file }
   its(:content) { should match /server 0.pool.ntp.org prefer/ }
   its(:content) { should match /server 1.pool.ntp.org/ }
 end
