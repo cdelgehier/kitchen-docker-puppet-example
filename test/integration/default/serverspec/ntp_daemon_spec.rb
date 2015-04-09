bac253f69e1e995ab07eb96f2a544f82b0e93b62
Delgehier Cédric <cedric.delgehier@gmail.com>
2015-04-10 00:58:21 +0200

    correction des ntp server

--- test/integration/default/serverspec/ntp_daemon_spec.rb
+++ test/integration/default/serverspec/ntp_daemon_spec.rb
@@ -40,3 +40,23 @@ describe "Ntp Daemon" do
     expect(service("ntpd")).to be_running
   end
 end
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe package('ntp'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe file('/etc/ntp.conf') do
+  it { should be_file }
+  its(:content) { should match /server 0.pool.ntp.org prefer/ }
+  its(:content) { should match /server 1.pool.ntp.org/ }
+end
+
+describe "Ntp Daemon" do
+  it "has a running service of ntpd" do
+    expect(service("ntpd")).to be_running
+  end
+end
